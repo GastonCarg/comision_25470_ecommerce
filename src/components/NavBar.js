b12fab1fe151a15357fff9f0ed32fe4108a1c1b3
GastonCarg <gastoncargnelutti@gmail.com>
2022-02-27 17:28:41 -0300

    Desafio 2 clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header id='main-header'>
+            <h1>Tienda de deportes</h1>
+            <nav>
+                <a href='#'>Indumentaria</a>
+                <a href='#'>Calzado</a>
+                <a href='#'>Accesorios</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
